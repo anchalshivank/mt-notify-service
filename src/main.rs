d6f041e43e3a35fc47ba0da723f1b2bcd6e420f0
Shivank Anchal <anchalshivank99@gmail.com>
2024-12-16 16:56:25 +0530

    Correct ci-cd.yml

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 use dotenv::dotenv;
-use futures::StreamExt;
 use notify_service::controllers::{clients, health, notify_machine, ClientMap};
-use ntex::web;
-use std::{collections::HashMap, sync::{Arc, Mutex}};
 use notify_service::services::websocket_service::ws_index;
+use ntex::web;
+use std::net::SocketAddr;
+use std::str::FromStr;
+use std::{collections::HashMap, sync::{Arc, Mutex}};
 
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     env_logger::init();
+
+
+    let port = std::env::var("PORT")
+        .unwrap_or_else(|_| "8081".to_string())
+        .parse::<u16>()
+        .expect("PORT must be a number!");
+
+    let auth_service_addr = std::env::var("AUTH_SERVICE_ADDR")
+        .unwrap_or_else(|_| "0.0.0.0:8080".to_string());
+    let auth_service_addr = SocketAddr::from_str(auth_service_addr.as_str())
+        .expect("Auth service address is not valid!");
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
 
     let clients_map: ClientMap = Arc::new(Mutex::new(HashMap::new()));
 
@@ -21,7 +35,7 @@ async fn main() -> std::io::Result<()> {
             .service(notify_machine)
             .service(clients)
     })
-        .bind("127.0.0.1:8080")?
+        .bind(addr)?
         .run()
         .await
 }
